fcebaaca1f41b98a08ff0224bd91426fcdbb8bf4
linwu <2929800706@qq.com>
2026-02-08 14:10:32 +0800

    Site updated: 2026-02-08 14:10:27

---
+++ js/theme/tags.js
@@ -0,0 +1,74 @@
+/* 
+ * hexo theme meow
+ * tag plugins scripts
+ */
+
+const initTags = () => {
+  /*
+   * transplant from hexo-theme-butterfly
+   */
+  const tabsFn = () => {
+    const navTabsElement = document.querySelectorAll(".tabs");
+    if (!navTabsElement.length) return;
+
+    const removeAndAddActiveClass = (elements, detect) => {
+      Array.from(elements).forEach(element => {
+        element.classList.remove("active");
+        if (element === detect || element.id === detect) {
+          element.classList.add("active");
+        }
+      });
+    };
+
+    const addTabNavEventListener = (item) => {
+      const navClickHandler = function (e) {
+        const target = e.target.closest("button");
+        if (target.classList.contains("active")) return;
+        removeAndAddActiveClass(this.children, target);
+        this.classList.remove("no-default");
+        const tabId = target.getAttribute("data-href");
+        const tabContent = item.querySelector(".tabs-content");
+        removeAndAddActiveClass(tabContent.children, tabId);
+      };
+      item.firstElementChild.addEventListener("click", navClickHandler);
+    };
+
+    const addTabToTopEventListener = item => {
+      const btnClickHandler = e => {
+        const target = e.target.closest("button");
+        if (!target) return;
+        meow.debounce(meow.scrollFn(meow.getActualTop(item) - 80), 300);
+      };
+      item.querySelector(".tabs-to-top").addEventListener("click", btnClickHandler);
+    };
+
+    navTabsElement.forEach(item => {
+      addTabNavEventListener(item);
+      addTabToTopEventListener(item);
+    });
+  };
+
+  tabsFn();
+};
+
+const initMusicPlayer = () => {
+  const playerTags = document.querySelectorAll(".music-tag");
+  if (!playerTags.length) return;
+
+  const activePlayer = (item) => {
+    const playerId = item.firstElementChild.id;
+    const playerFnName = "apFn_" + playerId.replace('music-player-', '');
+    for (let i = 0; i < window.apFnList.length; i++) {
+      if (playerFnName == window.apFnList[i].name) {
+        window.apFnList[i]();
+        break;
+      }
+    }
+  };
+
+  playerTags.forEach(item => {
+    activePlayer(item);
+  });
+};
+
+export { initTags, initMusicPlayer };
